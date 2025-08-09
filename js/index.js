0a976b9aa0a05a6516ad8a232fe1003896301de4
IvanSlivenko <ivan8822@ukr.net>
2025-08-09 14:04:53 +0300

    09-08-2025-14-04

---
+++ js/index.js
@@ -0,0 +1,20 @@
+"use strict"
+//==========================================
+
+// import { 
+//     getTasksLocalStorage,
+//     setTasksLocalStorage,
+//     generateUniqueId,
+//     initSortableList,
+//     updateListTasks
+// } from "./utils.js";
+
+const form = document.querySelector('.form');
+const textareaForm = document.querySelector('.form__textarea');
+const buttonSendForm = document.querySelector('.form__send-btn');
+const buttonCancel = document.querySelector('.form__cancel-btn');
+const output = document.querySelector('.output');
+let editId = null;
+let isEditTask = false;
+
+
